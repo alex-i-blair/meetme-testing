0dbb7834351f3f8e0cc6b82f4b8a832e18a66fe7
Alex Blair <bclan11@gmail.com>
2022-04-25 15:46:07 -0700

    render header test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,15 @@
 
-test('Should render the header', () => {
+import { render, screen, waitFor, watForElementToBeRemoved,} from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import App from './App'
 
-})
+describe('App', () => {
+  test('Should render the header', async () => {
+    render(
+        <App />
+    )
+    const header = await screen.findByRole('banner')
+    const headerText = await screen.findByText(/meet/i)
+  })
+});
+
